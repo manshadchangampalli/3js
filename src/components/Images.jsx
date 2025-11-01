c2113e3331b62bc1216e11b18749c09e3395f97e
Manshad Changampalli <manshadchangampalli010@gmail.com>
2025-11-01 08:11:49 +0400

    fix: the image changed

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -3,39 +3,42 @@ import { useFrame, useThree } from "@react-three/fiber";
 import { useRef } from "react";
 
 const Images = () => {
-    const {
-        viewport: { width, height },
-    } = useThree();
-    const groupRef = useRef();
-    const scroll = useScroll();
+  const {
+    viewport: { width, height },
+  } = useThree();
+  const groupRef = useRef();
+  const scroll = useScroll();
 
-    useFrame(() => {
-        if (groupRef?.current) {
-            groupRef.current.children[0].material.zoom = (1 + (scroll.range(0, 1 / 3)));
-        }
-    });
-    return (
-        <group ref={groupRef}>
-            <Image
-                scale={[width / 2, height]}
-                position={[-width / 4, 0, 0]}
-                url="./images/1.jpg"
-            />
-            <Image
-                position={[-width / 4, -height, 0]}
-                scale={[width / 4, height]}
-                url="./images/2.jpg"
-            />
-            <Image
-                position={[2, -height, 0]}
-                url="./images/3.jpg"
-            />
-            <Image
-                position={[0, -height * 2, 0]}
-                url="./images/4.jpg"
-            />
-        </group>
-    );
+  useFrame(() => {
+    if (groupRef?.current) {
+      groupRef.current.children[0].material.zoom = 1 + scroll.range(0, 1 / 3);
+      groupRef.current.children[3].material.position = [1 + scroll.range(2 / 3, 1 / 3), 0, 0];
+      console.log(scroll.range(2 / 3, 1 / 3))
+    }
+  });
+  return (
+    <group ref={groupRef}>
+      <Image
+        scale={[width / 2, height]}
+        position={[-width / 4, 0, 0]}
+        url="./images/1.jpg"
+      />
+      <Image
+        position={[-width / 4, -height, 0]}
+        scale={[width / 4, height]}
+        url="./images/2.jpg"
+      />
+      <Image
+        position={[0, -height, -2]}
+        scale={[width / 4, height]}
+        url="./images/3.jpg"
+      />
+      <Image
+        position={[0, -height * 2, 0]}
+        url="./images/4.jpg"
+      />
+    </group>
+  );
 };
 
 export default Images;
